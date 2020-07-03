0fae772327d3ce0ff7ef267c75ab2f1cff8fbe50
whtirado <whtirado@gmail.com>
2020-07-03 14:02:07 -0400

    Implemented header component and styles

---
+++ src/components/Header/js/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import '../css/header.css';
+
+export default function Header() {
+  return (
+    <header className='main-header accent-border'>
+      <div>Grocery List</div>
+    </header>
+  );
+}
