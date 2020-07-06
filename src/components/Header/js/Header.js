c57228a9ff8d195e65ca4d6c203f26287defe297
whtirado <whtirado@gmail.com>
2020-07-06 08:13:31 -0400

    Namespaced header scss

--- src/components/Header/js/Header.js
+++ src/components/Header/js/Header.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Header() {
   return (
     <header className='main-header'>
-      <div>Grocery List</div>
+      <div className='header-content'>
+        <div>Grocery List</div>
+      </div>
     </header>
   );
 }
