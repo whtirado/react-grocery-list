dbe4913368cff127a5b29b8e8eef1825089e40ff
whtirado <whtirado@gmail.com>
2020-07-06 09:14:50 -0400

    Moved created css folder

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/index.css';
 import App from './App';
+
+import './css/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
